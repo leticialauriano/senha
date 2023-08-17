0fcd90872e17b91774c0fa40b1de893e46274ab3
Letícia Lauriano <114084854+leticialauriano@users.noreply.github.com>
2023-08-17 12:51:37 -0300

    Adicionando projeto

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/*
+    Faça um programa que receba uma senha e teste se ela é igual à 904087. Se a senha estiver correta escreva “Senha Correta e Acesso permitido”, do contrário, escreva a mensagem “Você não tem acesso ao sistema."
+ */
+
+namespace Senha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string SENHACORRETA = "904087";
+
+            Console.WriteLine("--Programa Senha--\n");
+
+            Console.Write("Informe sua senha:");
+            string senha = Console.ReadLine();
+
+            if (senha == SENHACORRETA)
+            {
+                Console.WriteLine("Senha Correta.");
+                Console.WriteLine("Acesso permitido.");
+            }
+            else
+                Console.WriteLine("Você não tem acesso ao sistema.");                
+            
+            Console.WriteLine("\n--Pressione ENTER para encerrar--");
+            Console.ReadLine();
+        }
+    }
+}
